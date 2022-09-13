51748a17e6edc5753bb2df27d635545564526f88
Ahmed_Shalash <ifcodey@gmail.com>
2022-09-13 19:08:31 +0300

    done

--- Python/Fundamenrals/OOP/3-BankAccount/BankAccount.py
+++ Python/Fundamenrals/OOP/3-BankAccount/BankAccount.py
@@ -1,36 +1,46 @@
 
-class Users:
-
-    def __init__(self, name, email,balance = 0):
-        self.balance = 0
-        self.name = name
-        self.email = email
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate=0.01, balance=0):
+        self.int_rate = int_rate
         self.balance = balance
 
-    def make_deposit(self, amount):
+    def deposit(self, amount):
         self.balance += amount
         return self
 
-    def make_withdrawal(self, amount):
-        self.balance -= amount
+    def withdraw(self, amount):
+
+        if self.balance - amount > 0:  # if still there money in account after get some amount of money from it
+            # substracting a money amount from account beacuse still have a money in wallet
+            self.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            if self.balance > 5:
+                self.balance -= 5  # substracting a 5$ from
         return self
 
-    def display_user(self):
-        print(f"Name:({self.name}) , \
-balance:({self.balance}) , Email:({self.email})")
+        # ------------------------
+        # Solve by other way
+        # self.balance -=amount
+        # if self.balance <= amount:
+        #     self.balance-=5
+        #     print("Insufficient funds: Charging a $5 fee")
+        # return self
+        # ------------------------
+
+    def display_account_info(self):
+        print("Account", self.balance)
         return self
 
-    def transfer_money(self, User_2, amount):
-        self.balance -= amount
-        User_2.balance += amount
-        return self
+    def yield_interest(self):
+        if self.balance >= 1:
+            self.balance -= self.balance*self.int_rat  # (-) for substraction a interest of money from my account balance
+            return self
 
 
-U1 = Users("Marah","Marah@gmail.com",150)
-U2 = Users("Mohammad","Mohammad1990s@gmail.com",3000)
-U3 = Users("Semoin","Semoinba1990s@gmail.com",5000)
+Acc_1 = BankAccount(0.05, 0)
+Acc_2 = BankAccount(0.04, 200)
 
-U1.make_deposit(100).make_deposit(200).make_deposit(150).make_withdrawal(500).display_user()
-U2.make_deposit(100).make_deposit(200).make_withdrawal(500).make_withdrawal(500).display_user()
-U3.make_deposit(1000).make_withdrawal(500).make_withdrawal(500).make_withdrawal(500).display_user().transfer_money(U1, 150).display_user()
-
+Acc_1.deposit(20).deposit(10).deposit(20).withdraw(100).display_account_info()
+Acc_2.deposit(30).deposit(40).withdraw(10).withdraw(20).withdraw(15).withdraw(15).display_account_info()
--- Python/Fundamenrals/OOP/4-Users with Bank Accounts/User_BankAccount.py
+++ Python/Fundamenrals/OOP/4-Users with Bank Accounts/User_BankAccount.py
@@ -1,36 +1,97 @@
 
 class Users:
 
-    def __init__(self,balance, name, email):
-        self.balance = 0
-        self.name = name
-        self.email = email
+    def __init__(self, balance, name):
         self.balance = balance
+        self.name = name
 
     def make_deposit(self, amount):
         self.balance += amount
-        return self
 
     def make_withdrawal(self, amount):
         self.balance -= amount
-        return self
 
     def display_user(self):
-        print(f"Name:({self.name}) , \
-balance:({self.balance}) , Email:({self.email})")
-        return self
+        print(f"The Name of User:{self.name} and \
+balance is :{self.balance}")
 
     def transfer_money(self, User_2, amount):
         self.balance -= amount
         User_2.balance += amount
+
+
+U1 = Users(150, "Marah")
+U2 = Users(3000, "Mohammad")
+U3 = Users(50000, "Semoin")
+
+U1.make_deposit(100)
+U1.make_deposit(200)
+U1.make_deposit(150)
+U1.make_withdrawal(500)
+U1.display_user()
+
+U2.make_deposit(100)
+U2.make_deposit(200)
+U2.make_withdrawal(500)
+U2.make_withdrawal(500)
+U2.display_user()
+
+
+U3.make_deposit(1000)
+U3.make_withdrawal(500)
+U3.make_withdrawal(500)
+U3.make_withdrawal(500)
+U3.display_user()
+
+U1.transfer_money(U3, 150)
+U3.display_user()
+
+# ****************************************
+
+class BankAccount:
+    # don't forget to add some default values for these parameters!
+    def __init__(self, int_rate=0.01, balance=0):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
         return self
 
+    def withdraw(self, amount):
 
-U1 = Users(150, "Marah","Marah@gmail.com")
-U2 = Users(3000, "Mohammad","Mohammad1990s@gmail.com")
-U3 = Users(50000, "Semoin","Semoinba1990s@gmail.com")
+        if self.balance - amount > 0:  # if still there money in account after get some amount of money from it
+            # substracting a money amount from account beacuse still have a money in wallet
+            self.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            if self.balance > 5:
+                self.balance -= 5  # substracting a 5$ from
+        return self
 
-U1.make_deposit(100).make_deposit(200).make_deposit(150).make_withdrawal(500).display_user()
-U2.make_deposit(100).make_deposit(200).make_withdrawal(500).make_withdrawal(500).display_user()
-U3.make_deposit(1000).make_withdrawal(500).make_withdrawal(500).make_withdrawal(500).display_user().transfer_money(U1, 150).display_user()
+        # ------------------------
+        # Solve by other way
+        # self.balance -=amount
+        # if self.balance <= amount:
+        #     self.balance-=5
+        #     print("Insufficient funds: Charging a $5 fee")
+        # return self
+        # ------------------------
+
+    def display_account_info(self):
+        print("Account", self.balance)
+        return self
+
+    def yield_interest(self):
+        if self.balance >= 1:
+            self.balance -= self.balance*self.int_rat  # (-) for substraction a interest of money from my account balance
+            return self
+
+
+Acc_1 = BankAccount(0.05, 0)
+Acc_2 = BankAccount(0.04, 200)
+
+Acc_1.deposit(20).deposit(10).deposit(20).withdraw(100).display_account_info()
+Acc_2.deposit(30).deposit(40).withdraw(10).withdraw(20).withdraw(15).withdraw(15).display_account_info()
+
 
