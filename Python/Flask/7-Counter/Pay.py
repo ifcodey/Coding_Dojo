9dbbb6e64020478536889232b2560961cdc6c55c
Ahmed_Shalash <ifcodey@gmail.com>
2022-09-19 22:34:15 +0300

    done

--- Python/Flask/7-Counter/Pay.py
+++ Python/Flask/7-Counter/Pay.py
@@ -1,34 +1,46 @@
-from flask import Flask, render_template, request, redirect
+from distutils.log import debug
+from webbrowser import get
+from flask import Flask, render_template, session, redirect
+app = Flask(__name__)
 
-app = Flask(__name__)  # name of the page
-
-
-@app.route('/')
-def forms():
-    return render_template("form.html")
+app.secret_key = 'Keep it scret,keep it safe'
 
 
-@app.route("/", methods=['POST'])
-def initial_render():
-    print("Got Post Info")
-    print(request.form)
-
-    # yourName_form_form = request.form.get['name'] -- should but (Name)_form_form to work correct
-    # [ Or ]
-    yourName = request.form.get('name')
-    cityName = request.form.get('city')
-    langName = request.form.get('language')
-    textArea = request.form.get('textarea')
-    radio = request.form.get('radio')
-    checkbox = request.form.get('checkbox')
-
-    return render_template("result.html", yourNames=yourName, cityNames=cityName, langNames=langName, textAreas=textArea, radio=radio , checkbox=checkbox)
+@app.route("/counter")
+def index():
+    # if count in page equal value so increase a value to it but if i do
+    # refersh to page or the count = 1 return to open a counter html page.
+    if 'count' in session:
+        session['count'] = session.get('count') + 1
+    else:
+        session['count'] = 1
+    return render_template("counter.html")
 
 
-@app.route("/result")
-def showTab():
-    return redirect("form.html")
+@app.route("/")
+def index2():
+    if 'count2' in session:
+        session['count2'] = session.get('count2') + 1
+    else:
+        session['count2'] = 1
+    return render_template("counter.html")
 
+
+@app.route('/destroy')
+def destroy_session():
+    session.pop('count', None)
+    return redirect('/counter')
+
+
+@app.route('/increment')
+def increment():
+    session['count'] = int(session.get('count')) + 1
+    return redirect('/counter')
+
+@app.route('/reset')
+def reset():
+    session['count'] = -1
+    return redirect('/counter')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
