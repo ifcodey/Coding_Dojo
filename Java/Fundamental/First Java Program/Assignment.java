5166c4a4393830dcdab17f86c9df4457f757c055
Ahmed_Shalash <ifcodey@gmail.com>
2022-10-24 12:57:50 +0300

    done

---
+++ Java/Fundamental/First Java Program/Assignment.java
@@ -0,0 +1,10 @@
+
+public class Assignment {
+    public static void main(String[] args) {
+        System.out.println("My Name : Ahmed Shalash");
+        System.out.println("My Age : 23 yr");
+        System.out.println("My Location : Ramallah");
+
+    }
+
+}
