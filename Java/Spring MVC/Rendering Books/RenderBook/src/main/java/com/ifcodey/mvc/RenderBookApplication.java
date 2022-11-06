23611af00f068d1918ef0d7b545c68576859cee3
Ahmed_Shalash <ifcodey@gmail.com>
2022-11-06 15:00:51 +0200

    done

---
+++ Java/Spring MVC/Rendering Books/RenderBook/src/main/java/com/ifcodey/mvc/RenderBookApplication.java
@@ -0,0 +1,13 @@
+package com.ifcodey.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderBookApplication.class, args);
+	}
+
+}
