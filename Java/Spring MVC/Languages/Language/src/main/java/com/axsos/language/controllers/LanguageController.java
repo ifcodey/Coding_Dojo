6ddf5258c65248ec5ccd27d85d47b0534512bc87
ifcodey <ifcodey@gmail.com>
2022-11-09 20:44:36 +0200

    done

--- Java/Spring MVC/Languages/Language/src/main/java/com/axsos/language/controllers/LanguageController.java
+++ Java/Spring MVC/Languages/Language/src/main/java/com/axsos/language/controllers/LanguageController.java
@@ -1,5 +1,87 @@
 package com.axsos.language.controllers;
 
+import com.axsos.language.models.MLanguage;
+import com.axsos.language.services.LanguageService;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+//..
+
+//@RestController
+@Controller
 public class LanguageController {
+	@Autowired
+	private final LanguageService languageService;
+
+	public LanguageController(LanguageService languageService) {
+		this.languageService = languageService;
+	}
+
+	@GetMapping("/")
+	public String loads(Model model, @ModelAttribute("language") MLanguage language) {
+
+		model.addAttribute("all_language", languageService.allLanguage());
+		return "dashboard.jsp";
+	}
+
+	@GetMapping("/languages")
+	public String index(Model model, @ModelAttribute("language") MLanguage language) {
+		List<MLanguage> allObj = languageService.allLanguage();
+		model.addAttribute("all_language", allObj);
+		return "index.jsp";
+	}
+
+	@PostMapping("/languages")
+	public String addExpense(@Valid @ModelAttribute("language") MLanguage language, BindingResult result) {
+		if (result.hasErrors()) {
+			return "/dashboard.jsp";
+		} else {
+			languageService.createLanguage(language);
+			return "redirect:/languages";
+		}
+	}
+
+	@GetMapping("/edit/{id}")
+	public String showEditExpense(@PathVariable("id") Long id, Model model) {
+		MLanguage languageToEdit = languageService.findLanguage(id);
+		model.addAttribute("languageToEdit", languageToEdit);
+		return "/edit.jsp";
+	}
+
+	@PutMapping("/edit/{id}")
+	public String editExpense(@Valid @ModelAttribute(value = "languageToEdit") MLanguage languageToEdit,
+			BindingResult result, @PathVariable(value = "id") long id) {
+		if (result.hasErrors()) {
+			return "/edit.jsp";
+		} else {
+			languageService.updateLanguage(id, languageToEdit);
+			return "redirect:/languages";
+		}
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String destroy(@PathVariable("id") Long id) {
+		languageService.deleteLanguage(id);
+		return "redirect:/";
+	}
+
+	@GetMapping("/show/{id}")
+	public String showExpense(@PathVariable("id") Long id, Model model) {
+		MLanguage language = languageService.findLanguage(id);
+		model.addAttribute("language", language);
+		return "/expence.jsp";
+	}
 
 }
--- Java/Spring MVC/Languages/Language/src/main/java/com/axsos/language/repositories/LanguageRepository.java
+++ Java/Spring MVC/Languages/Language/src/main/java/com/axsos/language/repositories/LanguageRepository.java
@@ -1,5 +1,22 @@
 package com.axsos.language.repositories;
 
-public class LanguageRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import com.axsos.language.models.MLanguage;
+
+import java.util.*;
+
+@Repository
+public interface LanguageRepository extends CrudRepository<MLanguage, Long>{
+	List<MLanguage> findAll();
+
+//	// this method finds books with descriptions containing the search string
+	List<MLanguage> findByDescriptionContaining(String search);
+//
+//	// this method counts how many titles contain a certain string
+	Long countByNameContaining(String search);
+//
+//	// this method deletes a book that starts with a specific title
+	Long deleteByNameStartingWith(String search);
 }
