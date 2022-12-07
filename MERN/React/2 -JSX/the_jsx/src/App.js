372f2a347d0b01aeaeefd6c4646911733dd27b3c
ifcodey <ifcodey@gmail.com>
2022-12-07 22:07:43 +0200

    done react

---
+++ MERN/React/2 -JSX/the_jsx/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CMainPage from "./components/CMainPage";
+
+function App() {
+  return (
+    <div className="App">
+      <CMainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN/React/2 -JSX/the_jsx/src/components/CMainPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class CMainPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h4>Things I need to do :</h4>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Rum a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default CMainPage;
